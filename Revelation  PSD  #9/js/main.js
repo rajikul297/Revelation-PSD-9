2d851fbeb83ee799b0ba07997f86ccc81c3d3376
rajikul297 <111039297+rajikul297@users.noreply.github.com>
2022-09-07 23:18:03 +0600

    Add files via upload

---
+++ Revelation  PSD  #9/js/main.js
@@ -0,0 +1,9 @@
+
+//MOBILE-MENU OPEN CLOSE (PART 23)
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
